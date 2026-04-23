dbd25913ad31e54c19c3792e2a6403b9ea313873
Soda-dyosa <pheasblr@gmail.com>
2026-04-23 14:20:20 +0200

    updated structure

--- src/main/java/easv/gui/MainApp.java
+++ src/main/java/easv/gui/MainApp.java
@@ -5,7 +5,7 @@ import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class Exploding_KittensApp extends Application {
+public class MainApp extends Application {
 
     @Override
     public void start(Stage primaryStage) {
