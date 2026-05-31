d536a4ab19af4f007d3ace7268822db6066fbff7
Nemuxhin <Nemuxhin@gmail.com>
2026-05-31 21:03:44 +0200

    Delete src directory

--- src/main/java/easv/dal/DataAccessException.java
+++
@@ -1,7 +0,0 @@
-package easv.dal;
-
-public class DataAccessException extends RuntimeException {
-    public DataAccessException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
